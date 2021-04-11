5fd37e0a957fd85c43925d94b838361df2fddbbd
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-04-11 18:23:08 -0300

    setup inicial

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const middlewares = require("./middlewares");
+const router = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(middlewares.logs);
+
+app.use(router);
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get("/", (request, response) => {
+  response.send();
+});
+
+app.use(middlewares.errors);
+
+app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
